dff2ea067cbeeb68892d9cb1554f048a605a2166
ferdyhaspin <ferdihaspin@gmail.com>
2020-03-24 11:50:56 +0700

    add infinite fragment

--- app/src/main/java/com/ferdyhaspin/navigationcomponent/ConfirmationFragment.kt
+++ app/src/main/java/com/ferdyhaspin/navigationcomponent/ConfirmationFragment.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_confirmation.*
 
 /**
@@ -13,12 +17,21 @@ import kotlinx.android.synthetic.main.fragment_confirmation.*
 class ConfirmationFragment : Fragment() {
 
     private lateinit var recipient: String
+    private lateinit var test: String
     private lateinit var money: Money
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         recipient = arguments?.getString("recipient").toString()
+        test = arguments?.getString("test").toString()
         money = arguments?.getParcelable("amount")!!
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(requireContext(), "onResume $this", Toast.LENGTH_LONG).show()
     }
 
     override fun onCreateView(
@@ -31,8 +44,32 @@ class ConfirmationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
         val amount = money.amount
-        val confirmationMessage = "You have sent $amount to $recipient"
+        val confirmationMessage = "You have sent $amount to $recipient, data test : $test"
         confirmation_message.text = confirmationMessage
+
+        btnNext.setOnClickListener {
+            val test = input_test.text.toString()
+            if (test.isNotEmpty()) {
+                val bundle = bundleOf(
+                    "recipient" to recipient,
+                    "amount" to money,
+                    "test" to test
+                )
+
+                navController.navigate(
+                    R.id.action_confirmationFragment_self,
+                    bundle
+                )
+            }
+        }
+
+        btnBack.setOnClickListener {
+            requireActivity().onBackPressed()
+        }
+
+
     }
 }
